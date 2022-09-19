5f6dcd05895bcebe2261c8bb58c3c761e9f256d9
joegagliardo <joey@me.com>
2022-09-19 23:17:31 +0000

    0.0.0

--- gcloud-spanner/run-query.sh
+++ gcloud-spanner/run-query.sh
@@ -1,1 +1,1 @@
-gcloud spanner databases execute-sql pets-db --instance=test-spanner-instance --sql='SELECT OwnerName, PetName, PetType, Breed FROM Owners join Pets on Owners.OwnerID = Pets.OwnerID' 
+gcloud spanner databases execute-sql pets-db --instance=test-spanner-instance --sql='SELECT o.OwnerName, p.PetName, p.PetType, p.Breed FROM Owners as o JOIN Pets AS p ON o.OwnerID = p.OwnerID' 
