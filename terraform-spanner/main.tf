076bb4284bda6ad2f6634898e02468b42b4963d8
drehnstrom <drehnstrom@users.noreply.github.com>
2022-10-13 13:55:27 -0400

    Update main.tf

--- terraform-spanner/main.tf
+++ terraform-spanner/main.tf
@@ -1,12 +1,9 @@
 resource "google_spanner_instance" "db-instance" {
+  name             = "terraform-spanner-instance"
   config           = "regional-${var.region}"
   display_name     = "TF Spanner Instance"
   processing_units = var.processing_units
   force_destroy = var.force_destroy
-
-  labels = {
-    "env" = "testing"
-  }
 }
 
 resource "google_spanner_database" "test-database" {
@@ -16,8 +13,7 @@ resource "google_spanner_database" "test-database" {
   deletion_protection = var.deletion_protection 
 
   ddl = [
-    "CREATE TABLE Owners (OwnerID STRING(MAX) NOT NULL, OwnerName STRING(MAX) NOT NULL) PRIMARY KEY (OwnerID)",
-    "CREATE TABLE Pets (PetID STRING(MAX) NOT NULL, OwnerID STRING(MAX) NOT NULL, PetType STRING(MAX) NOT NULL, PetName STRING(MAX) NOT NULL, Breed STRING(MAX) NOT NULL, CONSTRAINT FK_OwnerPet FOREIGN KEY (OwnerID) REFERENCES Owners (OwnerID)) PRIMARY KEY (PetID)",
-    "CREATE INDEX PetsByOwnerID ON Pets(OwnerID)",
+    "CREATE TABLE Owners (OwnerID STRING(36) NOT NULL, OwnerName STRING(MAX) NOT NULL) PRIMARY KEY (OwnerID)",
+    "CREATE TABLE Pets (PetID STRING(36) NOT NULL, OwnerID STRING(MAX) NOT NULL, PetType STRING(MAX) NOT NULL, PetName STRING(MAX) NOT NULL, Breed STRING(MAX) NOT NULL) PRIMARY KEY (PetID)",
   ]
 }
